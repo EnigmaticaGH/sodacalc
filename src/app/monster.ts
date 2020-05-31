bd8424617ddf53d8ce93cdd376897210ecae201d
EnigmaticaGH <eventhorizonsoundtracks@gmail.com>
2020-05-31 14:55:37 -0400

    add type designation

--- src/app/monster.ts
+++ src/app/monster.ts
@@ -1,29 +1,37 @@
-export class Monster {
-  private _hp: number
-  private _atk: number
-  private _room: string
-  private _name: string
+export type MonsterType = 'Normal' | 'Miniboss' | 'Boss';
 
-  constructor(name: string, hp: number, atk: number, room: string) {
-    this._name = name
-    this._hp = hp
-    this._atk = atk
-    this._room = room
-  }
+export class Monster {
+  private _hp: number;
+  private _atk: number;
+  private _room: string;
+  private _type: MonsterType;
+  private _name: string;
+
+  constructor(name: string, hp: number, atk: number, room: string, type: MonsterType) {
+    this._name = name;
+    this._hp = hp;
+    this._atk = atk;
+    this._room = room;
+    this._type = type;
+  };
 
   get name(): string {
     return this._name
-  }
+  };
 
   get room(): string {
     return this._room
-  }
+  };
+
+  get type(): MonsterType {
+    return this._type
+  };
 
   get hp(): number {
     return this._hp
-  }
+  };
 
   get atk(): number {
     return this._atk
-  }
+  };
 }
