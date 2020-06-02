ac5b852ccafb751e09fa91f6b400f519727f98af
EnigmaticaGH <eventhorizonsoundtracks@gmail.com>
2020-06-02 18:56:30 -0400

    add route for character stats

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BestiaryComponent } from './bestiary/bestiary.component';
 import { MonsterStatCalcComponent } from './monster-stat-calc/monster-stat-calc.component';
+import { CharacterStatsComponent } from './character-stats/character-stats.component';
 
 
 const routes: Routes = [
   { path: 'bestiary', component: BestiaryComponent },
   { path: 'monster-stats', component: MonsterStatCalcComponent },
+  { path: 'character-stats', component: CharacterStatsComponent },
 ];
 
 @NgModule({
