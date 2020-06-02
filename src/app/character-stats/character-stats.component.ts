da8dcb0e8467861f8f42b32c4288ce2c17763310
EnigmaticaGH <eventhorizonsoundtracks@gmail.com>
2020-06-02 18:52:35 -0400

    begin character stats page feature

---
+++ src/app/character-stats/character-stats.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-character-stats',
+  templateUrl: './character-stats.component.html',
+  styleUrls: ['./character-stats.component.css']
+})
+export class CharacterStatsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
