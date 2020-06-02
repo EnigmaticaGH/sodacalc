ec8b0d7528a7ebd4459d85d23f008593c91be523
EnigmaticaGH <eventhorizonsoundtracks@gmail.com>
2020-06-02 18:57:01 -0400

    add route for relic stats

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BestiaryComponent } from './bestiary/bestiary.component';
 import { MonsterStatCalcComponent } from './monster-stat-calc/monster-stat-calc.component';
+import { RelicStatsComponent } from './relic-stats/relic-stats.component';
 
 
 const routes: Routes = [
   { path: 'bestiary', component: BestiaryComponent },
   { path: 'monster-stats', component: MonsterStatCalcComponent },
+  { path: 'relic-stats', component: RelicStatsComponent },
 ];
 
 @NgModule({
